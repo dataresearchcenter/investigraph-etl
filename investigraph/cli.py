83f7d127e77e1b2ff83786329821793dc1814e0e
Simon Wörpel <simon.woerpel@pm.me>
2025-06-02 23:11:23 +0200

    ✨ (cli) Add more stages and config uri via ENV

--- investigraph/cli.py
+++ investigraph/cli.py
@@ -1,21 +1,48 @@
-from typing import Annotated, Optional
+from typing import Annotated, Optional, TypeAlias
 
 import typer
 from anystore.cli import ErrorHandler
-from anystore.io import IOFormat, smart_stream_data, smart_write, smart_write_data
+from anystore.io import (
+    IOFormat,
+    smart_stream_data,
+    smart_stream_json_models,
+    smart_write,
+    smart_write_data,
+    smart_write_models,
+)
 from anystore.logging import configure_logging, get_logger
-from ftmq.io import smart_write_proxies
+from anystore.types import Uri
+from ftmq.io import smart_read_proxies, smart_write_proxies
 from rich import print
 
+from investigraph.exceptions import ImproperlyConfigured
 from investigraph.inspect import inspect_config
 from investigraph.logic.transform import transform_record
-from investigraph.model.context import get_dataset_context
+from investigraph.model.context import get_dataset_context, get_source_context
+from investigraph.model.source import Source
 from investigraph.pipeline import run
 from investigraph.settings import VERSION, Settings
 
 settings = Settings()
 cli = typer.Typer(no_args_is_help=True, pretty_exceptions_enable=settings.debug)
 log = get_logger(__name__)
+
+CONFIG_URI: TypeAlias = Annotated[
+    str | None,
+    typer.Option("-c", help="Any local or remote json or yaml uri"),
+]
+
+
+class ConfigUri(ErrorHandler):
+    def __init__(self, config_uri: Uri | None = None, *args, **kwargs):
+        uri = config_uri or settings.config
+        if not uri:
+            raise ImproperlyConfigured("Specify config.yml either via `-c` flag or ENV")
+        self.config_uri = uri
+        super().__init__(*args, **kwargs)
+
+    def __enter__(self):
+        return self.config_uri
 
 
 @cli.callback(invoke_without_command=True)
@@ -30,10 +57,7 @@ def cli_version(
 
 @cli.command("run")
 def cli_run(
-    config: Annotated[
-        str,
-        typer.Option("-c", help="Any local or remote json or yaml uri"),
-    ],
+    config: CONFIG_URI = None,
     store_uri: Annotated[Optional[str], typer.Option(...)] = None,
     index_uri: Annotated[Optional[str], typer.Option(...)] = None,
     entities_uri: Annotated[Optional[str], typer.Option(...)] = None,
@@ -41,35 +65,65 @@ def cli_run(
     """
     Execute a dataset pipeline
     """
-    print(
-        run(config, store_uri=store_uri, entities_uri=entities_uri, index_uri=index_uri)
-    )
+    with ConfigUri(config) as config_uri:
+        print(
+            run(
+                config_uri,
+                store_uri=store_uri,
+                entities_uri=entities_uri,
+                index_uri=index_uri,
+            )
+        )
+
+
+@cli.command("seed")
+def cli_seed(
+    config: CONFIG_URI = None,
+    out_uri: Annotated[str, typer.Option("-o")] = "-",
+    output_format: Annotated[IOFormat, typer.Option()] = IOFormat.json,
+):
+    """
+    Execute a dataset pipelines seed stage and write sources to out_uri
+    (default: stdout)
+    """
+    with ConfigUri(config) as config_uri:
+        ctx = get_dataset_context(config_uri)
+        sources = (c.source for c in ctx.get_sources())
+        smart_write_models(out_uri, sources, output_format=output_format.name)
 
 
 @cli.command("extract")
 def cli_extract(
-    config: Annotated[
-        str,
-        typer.Option("-c", help="Any local or remote json or yaml uri"),
-    ],
+    config: CONFIG_URI = None,
+    from_stdin: Annotated[bool, typer.Option()] = False,
     out_uri: Annotated[str, typer.Option("-o")] = "-",
     output_format: Annotated[IOFormat, typer.Option()] = IOFormat.json,
+    limit: Annotated[
+        int | None,
+        typer.Option("-l", help="Only get this number of items"),
+    ] = None,
 ):
     """
     Execute a dataset pipelines extract stage and write records to out_uri
     (default: stdout)
     """
-    with ErrorHandler():
-        ctx = get_dataset_context(config)
-        smart_write_data(out_uri, ctx.extract_all(), output_format=output_format.name)
+    with ConfigUri(config) as config_uri:
+        if from_stdin:
+            for source in smart_stream_json_models("-", Source):
+                ctx = get_source_context(config_uri, source.name)
+                smart_write_data(
+                    out_uri, ctx.extract(limit), output_format=output_format.name
+                )
+        else:
+            ctx = get_dataset_context(config_uri)
+            smart_write_data(
+                out_uri, ctx.extract_all(limit), output_format=output_format.name
+            )
 
 
 @cli.command("transform")
 def cli_transform(
-    config: Annotated[
-        str,
-        typer.Option("-c", help="Any local or remote json or yaml uri"),
-    ],
+    config: CONFIG_URI = None,
     in_uri: Annotated[str, typer.Option("-i")] = "-",
     out_uri: Annotated[str, typer.Option("-o")] = "-",
     input_format: Annotated[IOFormat, typer.Option()] = IOFormat.json,
@@ -78,27 +132,37 @@ def cli_transform(
     Execute a dataset pipelines transform stage with records from in_uri
     (default: stdin) and write proxies to out_uri (default: stdout)
     """
-    with ErrorHandler():
+    with ConfigUri(config) as config_uri:
 
         def _proxies():
             for ix, record in enumerate(
                 smart_stream_data(in_uri, input_format=input_format.name)
             ):
-                yield from transform_record(config, record, ix)
+                yield from transform_record(config_uri, record, ix)
 
         smart_write_proxies(out_uri, _proxies())
 
 
-@cli.command("inspect")
-def cli_inspect(
-    config: Annotated[
-        str,
-        typer.Option("-c", help="Any local or remote json or yaml uri"),
-    ],
+@cli.command("load")
+def cli_load(
+    config: CONFIG_URI = None,
+    in_uri: Annotated[str, typer.Option("-i")] = "-",
 ):
+    """
+    Execute a dataset pipelines load stage with proxies from in_uri
+    (default: stdin)
+    """
+    with ConfigUri(config) as config_uri:
+        proxies = smart_read_proxies(in_uri)
+        ctx = get_dataset_context(config_uri)
+        ctx.load(proxies)
+
+
+@cli.command("inspect")
+def cli_inspect(config: CONFIG_URI = None):
     """Validate dataset config"""
-    with ErrorHandler():
-        result = inspect_config(config)
+    with ConfigUri(config) as config_uri:
+        result = inspect_config(config_uri)
         print(f"[bold green]OK[/bold green] `{config}`")
         print(f"[bold]dataset:[/bold] {result.dataset.name}")
         print(f"[bold]title:[/bold] {result.dataset.title}")
@@ -111,7 +175,8 @@ def cli_settings(
     """
     Show current settings
     """
-    if out_uri == "-":
-        print(settings)
-    else:
-        smart_write(out_uri, settings.model_dump_json().encode())
+    with ErrorHandler():
+        if out_uri == "-":
+            print(settings)
+        else:
+            smart_write(out_uri, settings.model_dump_json().encode())
--- investigraph/settings.py
+++ investigraph/settings.py
@@ -18,20 +18,38 @@ class Settings(BaseSettings):
         env_nested_delimiter="_",
         nested_model_default_partial_update=True,
         env_file=".env",
-        env_file_encoding="utf-8",
         extra="ignore",
     )
 
     debug: bool = Field(default=False, alias="debug")
+    """Enable debug mode (more error output)"""
 
     data_root: Path = DATA_ROOT
+    """Default data directory to store archive and json exports"""
+
+    config: str | None = None
+    """Use this config.yml globally"""
 
     seeder: str = "investigraph.logic.seed:handle"
+    """Use this seed handler globally"""
+
     extractor: str = "investigraph.logic.extract:handle"
+    """Use this extract handler globally"""
+
     transformer: str = "investigraph.logic.transform:map_ftm"
+    """Use this transform handler globally"""
+
     loader: str = "investigraph.logic.load:handle"
+    """Use this load handler globally"""
+
     exporter: str = "investigraph.logic.export:handle"
+    """Use this export handler globally"""
 
     archive: StoreModel = StoreModel(uri=DATA_ROOT / "archive")
+    """Remote file archive store"""
+
     cache: StoreModel = StoreModel(uri="memory:///")
+    """Runtime cache"""
+
     store_uri: str = Field(default="memory:///", alias="ftmq_store_uri")
+    """Statement store for entity aggregation"""
