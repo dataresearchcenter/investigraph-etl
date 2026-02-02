a84ce22b389b72fe3583c9544597da41a2b42301
Simon Wörpel <simon.woerpel@pm.me>
2026-02-02 14:30:29 +0100

    🔖 Bump version: 0.7.1 → 0.8.0

--- investigraph/settings.py
+++ investigraph/settings.py
@@ -5,7 +5,7 @@ from anystore.model import StoreModel
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-VERSION = "0.7.1"
+VERSION = "0.8.0"
 
 DATA_ROOT = Path(
     os.environ.get("INVESTIGRAPH_DATA_ROOT", Path.cwd() / "data")
